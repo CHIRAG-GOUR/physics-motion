1429c6ba5cf9cbf4561a75b3c4fe874b989f7c31
CHIRAG-GOUR <chiraggour@github.com>
2026-02-28 16:30:01 +0530

    feat: complete motion chapter redesign

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Scene3D from './components/Scene3D';
+import ChapterContent from './components/ChapterContent';
+
+function App() {
+  return (
+    <div className="w-screen h-screen relative overflow-hidden bg-blue-50/50">
+      {/* Full-Screen 3D Background */}
+      <div className="fixed inset-0 z-0 pointer-events-none">
+        <Scene3D />
+      </div>
+
+      {/* Continuously Scrolling Main Content Area */}
+      <div className="absolute inset-0 z-10 overflow-x-hidden overflow-y-auto custom-scrollbar">
+        <ChapterContent />
+      </div>
+    </div>
+  );
+}
+
+export default App;
